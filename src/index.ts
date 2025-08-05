9e15fe8a467d00bf49b33c1201ea0178cb61cdd9
Guilherme Sella <guilhermesella1974@gmail.com>
2025-08-05 09:37:55 -0300

    feat: add user entity

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
