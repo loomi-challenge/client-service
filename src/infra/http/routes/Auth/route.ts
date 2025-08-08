63b13ee21f374b3ac340e0e0a26fe8b77d8c03a0
Guilherme Sella <guilhermesella1974@gmail.com>
2025-08-08 01:13:55 -0300

    feat: create user validation

--- src/infra/http/routes/Auth/route.ts
+++ src/infra/http/routes/Auth/route.ts
@@ -5,6 +5,7 @@ import { AuthUserController } from "../../controllers/Auth/auth-user.controller"
 import { ConfirmUserController } from "../../controllers/Auth/confirm-user.controller";
 import { ResendConfirmationCodeController } from "../../controllers/Auth/resend-confirmation-code.controller";
 import { container } from "tsyringe";
+import { validateCreateUser } from "@/infra/validators/zod/create-user.validator";
 
 export const authRouter = Router();
 
@@ -18,7 +19,7 @@ const resendConfirmationCodeController = container.resolve(
 );
 
 authRouter.post("/login", expressAdaptRoute(authUserController));
-authRouter.post("/register", expressAdaptRoute(createUserController));
+authRouter.post("/register", validateCreateUser, expressAdaptRoute(createUserController));
 authRouter.post("/confirm-user", expressAdaptRoute(confirmUserController));
 authRouter.post(
   "/resend-confirmation-code",
